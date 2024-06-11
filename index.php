4e312e6a145fd5187139a028b5875b80f51ecdc2
ArthurMLi <12200069@aluno.cotemig.com.br>
2024-06-11 20:23:18 -0300

    Inicio

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MVC-CRUD</title>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet" />
+    <link href="fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"/>
+
+    <style type="text/css">
+    body {
+        margin: 20px;
+        background-color: #ffffff;
+    }
+    *{
+         font-family: 'Open Sans', sans-serif;
+    }
+    h2 { 
+        font-family: 'Open Sans', sans-serif;
+    }
+    .thead {
+        background-color: #f7f7f7;
+    }
+</style>
+</head>
+<body>
+    
+</body>
+</html>
